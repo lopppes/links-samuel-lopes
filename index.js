b705da1484888c30068f87985f1421d84381c92f
lopppes <sl263920@gmail.com>
2024-08-28 16:32:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+  }, 1000); 
+});
+
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll("[data-nav-toggler]"),
+  document.querySelectorAll("[data-nav-link]"),
+  document.querySelector("[data-navbar]"),
+  document.querySelector("[data-overlay]")
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", closeNav);
+
